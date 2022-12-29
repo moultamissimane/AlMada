785363126a8bf7aef2150db452c2559ea7da3605
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-12-29 11:58:50 +0100

    add spinner component

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="inline-block pb-3 w-10 h-10 border-2 border-current border-t-transparent text-red-400 rounded-full">
+      <span className="">Loading..</span>
+    </div>
+  );
+};
+
+export default Spinner;
