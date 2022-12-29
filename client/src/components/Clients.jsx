56482b1790f8b326e3f6fe6d91ae09cf73ba6fea
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-12-29 11:58:28 +0100

    add clients component

---
+++ client/src/components/Clients.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import ClientRow from "./ClientRow";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import Spinner from "./Spinner"; 
+const Clients = () => {
+  const { data, loading, error } = useQuery(GET_CLIENTS);
+
+  if (loading) return <Spinner />;
+  if (error) return `Error! ${error.message}`;
+  return (
+    <>
+      {!loading && !error && (
+        <div className="container py-2 inline-block min-w-full ">
+          <table className="container min-w-full">
+            <thead className="bg-gray-600">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.clients.map((client) => (
+                <ClientRow key={client.id} client={client} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Clients;
